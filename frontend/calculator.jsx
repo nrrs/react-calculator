5140e164a02af6187c52d9b42dbfb88e075be54f
Norris Kwan <norris.kwan@gmail.com>
2017-07-09 11:18:29 -0700

    Add calculator component

---
+++ frontend/calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'React';
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="calculator-container">
+        
+      </div>
+    );
+  }
+}
